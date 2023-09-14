0e3db5f325e2b8a26d5b3d44bc959adae8f9e0ae
luisburigo <guga.burigo29@outlook.com>
2023-09-14 20:08:36 -0300

    chore: example

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import logo from './assets/logo.svg'
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Image src={logo} width={30} alt="Bsafe logo"  />
+        <div style={{ marginLeft: 10 }}>SAFE</div>
+      </div>
+  ),
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
