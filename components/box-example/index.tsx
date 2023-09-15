e28184ae173e0d0f8f6615a082536fcb0aebf21d
luisburigo <guga.burigo29@outlook.com>
2023-09-14 21:43:03 -0300

    feat: create vault

---
+++ components/box-example/index.tsx
@@ -0,0 +1,15 @@
+const BoxExample = ({children}) => {
+  return (
+      <div style={{
+          width: '100%',
+          padding: 12,
+          border: '1px solid',
+          marginTop: 12,
+          marginBottom: 12
+      }}>
+          {children}
+      </div>
+  )
+}
+
+export {BoxExample}
