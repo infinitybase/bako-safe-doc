b887ffc5a1c5f7dad488a81f7d06c0904c0c1e31
luisburigo <guga.burigo29@outlook.com>
2024-08-22 21:56:13 -0300

    feat: add deploy rust example

--- snippets/rust-contract-deploy/src/main.rs
+++ snippets/rust-contract-deploy/src/main.rs
@@ -1,21 +1,28 @@
-use std::string::ToString;
+use fuel_tx::Transaction;
+use fuels::client::FuelClient;
 use fuels::prelude::{Contract, CreateTransactionBuilder, LoadConfiguration, Provider};
+use std::string::ToString;
 
 /// The API token to connect to Bako Gateway
 /// Generate at https://safe.bako.global/ in the vault setting
-const API_TOKEN: String = "".to_string();
-const BAKO_GATEWAY_URL: String = "https://api.bako.global/v1/graphql?api_token=".to_string();
+const API_TOKEN: &str = "";
+const BAKO_GATEWAY_URL: &str = "https://api.bako.global/v1/graphql?api_token=";
 
 #[tokio::main]
 async fn main() {
     // Connect to Bako Gateway with the API token
-    let provider = Provider::connect(format!("{BAKO_GATEWAY_URL}{api_token}")).await?;
+    let node_url = format!("{BAKO_GATEWAY_URL}{API_TOKEN}");
+    let client = FuelClient::new(node_url.clone()).expect("Failed to create FuelClient");
+    let provider = Provider::connect(node_url)
+        .await
+        .expect("Failed to connect to provider");
 
     // Load the contract from the binary file
     let contract = Contract::load_from(
         "../contracts/contract/out/release/contract.bin",
         LoadConfiguration::default(),
-    )?;
+    )
+    .expect("Failed to load contract");
 
     // Prepare the transaction for contract deployment
     let contract_id = contract.contract_id();
@@ -31,13 +38,22 @@ async fn main() {
         salt,
         storage_slots.to_vec(),
         Default::default(),
-    ).with_max_fee_estimation_tolerance(0.05);
+    )
+    .with_max_fee_estimation_tolerance(0.05);
 
-    let transaction = transaction_builder.build(provider).await?;
+    let transaction = transaction_builder
+        .build(provider.clone())
+        .await
+        .expect("Failed to build transaction");
+    let tx = Transaction::from(transaction);
 
     // Send the transaction to Bako
     println!("Sending tx to Bako...");
-    let tx_id = provider.send_transaction(transaction).await?;
+
+    let tx_id = client
+        .submit(&tx)
+        .await
+        .expect("Failed to submit transaction");
 
     println!("Transaction sent to Bako!");
     println!("- Contract ID: {}", contract_id);
